43dfccc8f400789013f6cd092d8f6f1b9695db7c
Sagar5687 <sagar220103@gmail.com>
2026-07-21 10:23:09 +0200

    ft_print_f

--- ft_printf.h
+++ ft_printf.h
@@ -7,7 +7,7 @@ int ft_vprintf(const char *format, va_list args);
 int ft_handle_format(const char *format, va_list args);
 int ft_print_pointer(void *ptr);
 int ft_print_unsigned(unsigned int num);
-int ft_print_hexadecimal(unsigned int num, char format);
+int	ft_print_hexadecimal(unsigned long num, char format);
 int ft_putchar_fd(char c);
 int ft_putstr_fd(char *s);
 int ft_putnbr_fd(int n);
